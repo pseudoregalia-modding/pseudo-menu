82de2492e7638d60eafb62eaf5cc83e6f2cb636f
turncoda <144861042+turncoda@users.noreply.github.com>
2024-03-06 22:03:05 -0800

    Migrate PseudoMenu to its own Unreal project

---
+++ deploy.bat
@@ -0,0 +1,2 @@
+copy "C:\Users\A\Documents\Unreal Projects\PseudoMenuProject\Windows\pseudoregalia\Content\Paks\pakchunk344-Windows.pak" "C:\Program Files (x86)\Steam\steamapps\common\Pseudoregalia\pseudoregalia\Content\Paks\PseudoMenu_NoMainMenuCopy_p.pak"
+pause
