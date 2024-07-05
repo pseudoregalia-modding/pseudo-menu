d9d1fb1553c4fc7e6972d7ab94cce2f503a678f5
turncoda <144861042+turncoda@users.noreply.github.com>
2024-07-04 17:14:13 -0700

    Automate current directory in cook.bat

--- cook.bat
+++ cook.bat
@@ -1,11 +1,8 @@
 @echo off
 
-REM Set this to be the folder containing the .uproject file.
-set uproject_folder=C:\Users\A\Documents\Unreal Projects\PseudoMenuProject
-
 REM Point this to your Unreal Engine installation. (You probably don't have to modify this.)
 set run_uat=C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat
 
-call "%run_uat%" BuildCookRun -project="%uproject_folder%\pseudoregalia.uproject" -platform=Win64 -cook -stage
+call "%run_uat%" BuildCookRun -project="%~dp0\pseudoregalia.uproject" -platform=Win64 -cook -stage
 
 pause
